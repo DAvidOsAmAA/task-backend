4e8dca45d16a0e46f1b95f10cd055a0a39d7a4e2
David Osama <davidosamamilad@gmail.com>
2025-02-14 23:28:03 +0200

    changes

--- database/db.connection.js
+++ database/db.connection.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
-export const dbConnection = mongoose.connect("mongodb+srv://davidosamamilad:12345david@cluster0.8t8gq.mongodb.net/taskDatabase").then(()=>{
+export const dbConnection =
+ mongoose.connect("mongodb+srv://davidosamamilad:12345david@cluster0.8t8gq.mongodb.net/taskDatabase").then(()=>{
 console.log("db connected successfully")
 }).catch(()=>{
 console.log("err conntection with the db")
