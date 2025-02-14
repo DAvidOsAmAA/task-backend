70509055704511f5cf3f97cd637cd359b169596d
David Osama <davidosamamilad@gmail.com>
2025-02-15 00:20:47 +0200

    changes in deploy

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 1 - you need to make npm i before start 
 2 - run project by nodemon
+3 - the link of vercel https://task-backend-73ji.vercel.app
--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use("/",(req,res)=>{
 })
 
 
-app.use("*",(req,res,next)=>{
+app.get("*",(req,res,next)=>{
 next(new Error("route not found"),)
 })
 
